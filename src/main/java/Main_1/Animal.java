d49563f290d687f8c6903c0f2cbddf4e2bee6942
BekhterNikolay <n.v.bekhter@mail.ru>
2022-04-12 19:05:52 +0300

    добавил класс Animal

--- src/main/java/Main_1/Animal.java
+++ src/main/java/Main_1/Animal.java
@@ -1,2 +1,15 @@
-package Main_1;public class Animal {
+package Main_1;
+
+public class Animal {
+    int age;
+    String name;
+
+    public Animal(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public void run(){
+        System.out.println(this.name + " пробежал");
+    }
 }
--- src/main/java/Main_1/Main.java
+++ src/main/java/Main_1/Main.java
@@ -1,2 +1,10 @@
-package Main_1;public class Main {
+package Main_1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Animal animal = new Animal(1, "Bobik");
+        animal.run();
+    }
 }
